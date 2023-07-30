0cf025fc6a8fd8523b55ef181fd94c9dba74cbf5
William Blackie <>
2023-07-30 21:22:39 +0100

    init

---
+++ bootstrap.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Installations
+# zsh
+sudo apt zsh
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+# zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+# pyenv
+curl https://pyenv.run | bash
+
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+# tmux
+sudo apt install tmux
+
+# i3
+sudo apt install i3
+
+# Sym links
+ln -s ~/.dotfiles/.bashrc ~/.bashrc
+ln -s .dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/i3/config ~/.config/i3/config
+
