dbb0ce389f40ae72d8f33d964e5e086fcec19bfa
William Blackie <>
2023-08-04 15:29:44 +0100

    Add posgtres

--- bootstrap.sh
+++ bootstrap.sh
@@ -32,6 +32,19 @@ autorandr --default laptop
 # flameshot
 sudo apt install flameshot
 
+# Postgresql
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+
+# Import the repository signing key:
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
+# Update the package lists:
+sudo apt-get update
+
+# Install the latest version of PostgreSQL.
+# If you want a specific version, use 'postgresql-12' or similar instead of 'postgresql':
+sudo apt-get -y install postgresql
+
 # Sym links
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
